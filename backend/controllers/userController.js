185264c91fa3b9159294afe265b1402da1470a2c
Samruddhi1404 <sam626240@gmail.com>
2024-02-18 10:20:29 +0530

    updated usercontroller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const Student = require("../models/studentModel");
 const User = require("../models/userModel");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
-const Student = require("./../models/studentModel");
+//const Student = require("./../models/studentModel");
 
 exports.getAll = catchAsync(async (req, res, next) => {
   const docs = await User.find({ type: "STUDENT" }).limit(3);
