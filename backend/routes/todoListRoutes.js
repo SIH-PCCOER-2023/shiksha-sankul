d772b23e7493f64b7de53d70579cbf947eff6d16
Sanikagharge <sanika.gharge04@gmail.com>
2024-04-03 23:08:27 +0530

    todolist backend

--- backend/routes/todoListRoutes.js
+++ backend/routes/todoListRoutes.js
@@ -5,11 +5,10 @@ const router = express.Router();
 
 router.route("/").get(todoListController.getAll).post(todoListController.createOne);
 
-router
-  .route("/:id")
-  .get(todoListController.getOne)
-  .patch(todoListController.updateOne)
-  .delete(todoListController.deleteOne);
+router.route("/:id").get(todoListController.getOne)
+router.route("/:todoId").patch(todoListController.updateOne).delete(todoListController.deleteOne);
+
+//router.route("/:userId/:todoId").patch(todoListController.updateOne).delete(todoListController.deleteOne);
 
 
 module.exports = router;
